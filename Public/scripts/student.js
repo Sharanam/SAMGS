be772f8acd43c5c8db7f56c2ddb11733dca88d05
38852691 <38852691+Sharanam@users.noreply.github.com>
2021-11-20 15:01:01 +0530

    Profile Done

---
+++ Public/scripts/student.js
@@ -0,0 +1,53 @@
+function tabChanger() {
+    event.preventDefault();
+    // active tab change
+    let d = document.getElementsByClassName("nav-link");
+    for (var i = 0; i < d.length; i++) {
+        d[i].classList.remove("active");
+    }
+    event.target.classList.add("active");
+
+    // respective content change
+    d = document.getElementsByClassName('tab-items');
+    for (let i = 0; i < d.length; i++) {
+        d[i].classList.remove("d-block");
+        d[i].classList.add("d-none");
+    }
+    d = document.getElementById(event.target.href.split("#")[1]);
+    d.classList.add("d-block");
+    d.children[0].click()
+}
+
+function getProfile() {
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            let data = JSON.parse(this.responseText);
+            if (!data.res) return false;
+            data = data.data;
+            document.getElementsByName('name')[0].value = data.name;
+            document.getElementsByName('address')[0].value = data.address;
+            document.getElementsByName('age')[0].value = data.age;
+            document.getElementsByName('class')[0].value = data.class;
+            document.getElementsByName('section')[0].value = data.section;
+        }
+    };
+    xhttp.open("GET", "../index.php?type=student&act=profile", true);
+    xhttp.send();
+}
+
+function setProfile(event) {
+    event.preventDefault();
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            res = JSON.parse(this.responseText);
+            alert(res.res ? "Profile Updated" : "Something went wrong.")
+        }
+    };
+    xhttp.open("POST", "../index.php?type=student&act=profile", true);
+    xhttp.send(new FormData(document.getElementById('profileForm')));
+}
+
+// on page load
+document.getElementById(document.getElementsByClassName("active")[0].href.split("#")[1]).children[0].click()
--- view/dashboard/student/index.php
+++ view/dashboard/student/index.php
@@ -1,12 +1,13 @@
+<script src="../Public/scripts/student.js" defer> </script>
 <div class="container">
     <div class="row">
         <div class="col-md-12 text-center">
             <ul class="nav nav-pills nav-fill">
                 <li class="nav-item">
-                    <a onclick="return tabChanger()" class="nav-link" href="#profile">Profile</a>
+                    <a onclick="return tabChanger()" class="nav-link active" href="#profile">Profile</a>
                 </li>
                 <li class="nav-item">
-                    <a onclick="return tabChanger()" class="nav-link active" href="#appointments">Appointment</a>
+                    <a onclick="return tabChanger()" class="nav-link " href="#appointments">Appointment</a>
                 </li>
                 <li class="nav-item">
                     <a onclick="return tabChanger()" class="nav-link" href="#history">History</a>
@@ -15,33 +16,56 @@
         </div>
     </div>
 </div>
-<script>
-    function tabChanger() {
-        // active tab change
-        let d = document.getElementsByClassName("nav-link");
-        for (var i = 0; i < d.length; i++) {
-            d[i].classList.remove("active");
-        }
-        event.target.classList.add("active");
-
-        // respective content change
-        d = document.getElementsByClassName('tab-items');
-        for (let i = 0; i < d.length; i++) {
-            d[i].classList.remove("d-block");
-            d[i].classList.add("d-none");
-        }
-        document.getElementById(event.target.href.split("#")[1]).classList.add("d-block");
-    }
-</script>
 <div class="container">
-    <div class="row tab-items d-none" id="profile">
-        <p> profiles- 2 APIs</p>
+    <div class="row tab-items d-block" id="profile">
+        <button class="d-none" onclick="getProfile()">Profile</button>
+        <div class="col-md-8 m-auto">
+            <h1 class="display-4 text-center">Update Profile</h1>
+            <form onsubmit="setProfile(event)" id='profileForm'>
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="name" class="form-control form-control-lg" placeholder="name" name="name" minlength="5" required />
+                </div>
+                <div class="form-group">
+                    <label for="address">Address</label>
+                    <input type="text" class="form-control form-control-lg" placeholder="address" name="address" required />
+                </div>
+                <div class="form-group">
+                    <label for="age">Age</label>
+                    <input type="number" class="form-control form-control-lg" placeholder="age" name="age" min="5" required />
+                </div>
+                <div class="form-group">
+                    <label for="Class">Class</label>
+                    <input type="number" class="form-control form-control-lg" placeholder="Class" name="class" required />
+                </div>
+                <div class="form-group">
+                    <label for="section">Section</label>
+                    <input type="text" maxlength="1" class="form-control form-control-lg" placeholder="section" name="section" required />
+                </div>
+                <div id="profile-content" class="error-message"></div>
+                <input type="submit" class="btn btn-info btn-block mt-4" name="update" />
+            </form>
+        </div>
     </div>
-    <div class="row tab-items d-block" id="appointments">
-        3 APIs - book and manage appointments (+1 to get list of advisors)
-        Pop-up in appointment
+    <div class="row tab-items d-none" id="appointments">
+        <button class="d-none" onclick="getAppointments()">Profile</button>
+        <div id="appointmentView">
+            <p>
+                Appointments will be shown here
+                <br />
+                For that, 3 APIs - book and manage appointments
+                <br />
+                (+1 to get list of advisors)
+                <br />
+                Pop-up in appointment
+            </p>
+        </div>
     </div>
     <div class="row tab-items d-none" id="history">
-        history - 1 API
+        <button class="d-none" onclick="getHistory()">Profile</button>
+        <div id="historyView">
+
+            <!-- <p> only API for history will be used and shown here </p> -->
+        </div>
     </div>
 </div>
