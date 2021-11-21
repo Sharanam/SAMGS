0a5b289ba16579c111094b65abf9e440e2c0a524
38852691 <38852691+Sharanam@users.noreply.github.com>
2021-11-21 11:42:07 +0530

    Landing Page Done

--- view/landing.php
+++ view/landing.php
@@ -5,17 +5,40 @@ require './common/navbar.php';
 
 
 <div class="landing">
-  <div class="dark-overlay landing-inner text-light">
-    <div class="container">
+  <div class="dark-overlay landing-inner text-light ">
+    <div class="container ">
       <div class="row">
         <div class="col-md-12 text-center">
           <h1 class="display-3 mb-4"><b>Student Academic Misconduct Guidance Service</b></h1>
           <p class="lead"><b>Student Information Center</b></p>
+          <hr style="background-color:red ;" />
+          <div class="text-justify my-4 overflow-auto">
+            <h2 class="text-center">The Academic Misconduct Procedure</h2>
+            <h3>Reporting and investigation</h3>
+            <p>
+              Heads of School/Department will appoint Academic Misconduct Officers to undertake investigations into allegations of academic misconduct on behalf of the School/Department. Decision-making staff will be appropriately trained, enabling the consistent and fair application of these Regulations.
+            </p>
+            <p>
+              In cases of suspected <b>misconduct in an examination</b>, the invigilator will write a report and the examination script will be annotated to indicate the point at which the suspected misconduct was identified. This report will be forwarded to the Academic Misconduct Officer in which the work was undertaken.
+            </p>
+            <p>
+              In cases of <b>false authorship</b>, the reporting staff member is expected to outline their reasoning for suspecting that the student is not the author of the work submitted and provide any pertinent evidence. This may include (but is not limited to): inconsistency in writing style, language and grammar; the assignment not properly addressing the question set; inappropriate referencing and bibliography. This may also involve inconsistency in grades/standard of work, and allegations from third parties.
+            </p>
+            <p>
+              Where a student has reason to <b>suspect a fellow student</b> of academic misconduct, they may report this, to the Academic Misconduct Officer in which the work was undertaken. Anonymity cannot be guaranteed as the accused student has a right of access to information pertaining to their personal data. If the reporting student does not give their consent for their identity to be revealed, no further action can be taken, unless additional and/or compelling evidence is identified by an academic member of staff.
+            </p>
+            <a href="https://www.nottingham.ac.uk/academicservices/currentstudents/academic-misconduct.aspx" target="_blank" class="btn bg-danger text-white">Read More...</a>
+            <hr style="background-color:red ;" class="mt-4" />
+            <h2 class="text-center">Student Complaints and Academic Appeals</h2>
+            <h3>Complaints and Academic Appeals</h3>
+            <p>We want all of our students to get the most out of their time at the University of&nbsp;Nottingham&nbsp;and if things are getting in the way of this, it is very important that you let us know. Ideally you should speak to&nbsp;someone about your concerns, e.g.&nbsp;your&nbsp;personal tutor, course lead or Student Services, to give them the chance to understand&nbsp;the issue&nbsp;and offer any support&nbsp;or advice&nbsp;they can. &nbsp;</p>
+            <p>Where this isn’t possible, or hasn’t fixed the problem, then you may wish to&nbsp;consider the Academic Appeals or Student Complaints procedures.&nbsp;You need to be aware that there are strict timescales for bringing complaints and appeals, details can be found in the relevant procedures.</p>
+            <p> If you intend to make an appeal or raise a complaint but are concerned you will not be able to comply with the submission timescales, then you should request an extension to the deadline by using the generic appeal or complaint email addresses found below.</p>
+            <a href="https://www.nottingham.ac.uk/academicservices/currentstudents/academicappealsmisconduct.aspx" target="_blank" class="btn bg-danger text-white">Read More...</a>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </div>
-<?php
-require './common/footer.php';
-?>
+<?php require './common/footer.php'; ?>
